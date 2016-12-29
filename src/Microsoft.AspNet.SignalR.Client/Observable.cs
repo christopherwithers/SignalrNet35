447dfbafbbd78b074a6ed8d82094a1e4a82b58f0
Chris <chris.withers@gmail.com>
2016-12-29 08:32:22 +0000

    init rep

---
+++ src/Microsoft.AspNet.SignalR.Client/Observable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Microsoft.AspNet.SignalR.Client
+{
+
+    public interface IObservable<out T>
+    {
+        IDisposable Subscribe(IObserver<T> observer);
+    }
+    public interface IObserver<in T>
+    {
+        void OnNext(T value);
+        void OnError(Exception error);
+        void OnCompleted();
+    }
+
+}
